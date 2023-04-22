6dc7ccd51b52957d823604c43040197419d66575
BorysSabov <92867567+BorysSabov@users.noreply.github.com>
2023-04-22 05:56:48 +0000

    Deploying to gh-pages from @ BorysSabov/goit-js-hw-09@fbc5046d4241a9087fb1502b2785e882eb0d1cde 🚀

---
+++ 01-color-switcher.72acd709.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),o=document.querySelector("[data-stop]");console.log(e),console.log(o),e.addEventListener("click",(function(){e.disabled=!0,o.disabled=!1,t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),o.addEventListener("click",(function(){e.disabled=!1,o.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.72acd709.js.map
